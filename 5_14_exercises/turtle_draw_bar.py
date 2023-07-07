0a93ba9b134b5f074676df5aa38126d4f3f300a8
sergey vardanyan <sergey.vardanyan@volusion.com>
2023-07-07 18:01:42 -0400

    Add exercise 7, 8, 9

--- 5_14_exercises/turtle_draw_bar.py
+++ 5_14_exercises/turtle_draw_bar.py
@@ -8,8 +8,16 @@ def draw_bar(t, height):
     t.begin_fill()
     t.left(90)
     t.forward(height)
-    t.right(90)
-    t.write(f"  {height}", font=("Arial", 20, "normal"))
+    if height > 0:
+        t.right(90)
+        t.write(f" {height}", font=("Arial", 18, "normal"))
+    else:
+        t.penup()
+        t.forward(-21)
+        t.write(f"{height}", font=("Arial", 18, "normal"))
+        t.back(-21)
+        t.pendown()
+        t.right(90)
     t.forward(40)
     t.right(90)
     t.forward(height)
@@ -24,15 +32,22 @@ wn = turtle.Screen()
 wn.bgcolor('lightgreen')
 
 tess = turtle.Turtle()
+tess.speed(8)
 tess.color('blue', 'red')
 tess.pensize(3)
 tess.penup()
-tess.setpos((-150, -70))
+tess.setpos((-200, -70))
 tess.pendown()
 
-xs = [48, 117, 200, 240, 160, 260, 220]
+xs = [48, -117, -200, 240, 160, 260, 220, -20]
 
 for a in xs:
+    if a >= 200:
+        tess.color('blue', 'red')
+    elif a >= 100 and a < 200:
+        tess.color('blue', 'yellow')
+    elif a < 100:
+        tess.color('blue', 'green')
     draw_bar(tess, a)
 
 wn.mainloop()
